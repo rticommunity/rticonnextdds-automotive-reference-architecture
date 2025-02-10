5972be47ba3e82e8e6ce374a41822d12ca82c0c9
Maruel Zain <mzain@rti.com>
2025-02-10 16:06:09 +0100

    Added visualization for window state and easier operation

--- cockpit.py
+++ cockpit.py
@@ -62,6 +62,11 @@ class Cockpit:
         self.thread_reader.start()
     
 
+    def act_on_samples(self, samples):
+        "Dummy function, to be overridden by application."
+        print(samples)
+    
+
     def reader_monitor(self):
         "Dedicated thread target for subscribing to updates on the reader topic."
 
@@ -76,7 +81,7 @@ class Cockpit:
                     # Take any samples that we have not seen before
                     new_samples = self.reader.select().condition(self.read_condition).take_data()
                     # Take action
-                    print(new_samples)
+                    self.act_on_samples(new_samples)
     
 
     def _send_target(self, window_id, target):
@@ -102,16 +107,39 @@ if __name__ == "__main__":
         import tkinter as tk
         from tkinter import ttk
         root = tk.Tk()
-        frame = ttk.Frame(root)
-        label = ttk.Label(frame, text="Window id: ")
-        entry = ttk.Entry(frame, text="FR")
-        btn_open = ttk.Button(frame, text="OPEN", command=lambda: app.send_open(entry.get()))
-        btn_close = ttk.Button(frame, text="CLOSE", command=lambda: app.send_close(entry.get()))
-        label.pack(side=tk.LEFT)
-        entry.pack(side=tk.LEFT)
-        btn_open.pack(side=tk.LEFT)
-        btn_close.pack(side=tk.LEFT)
-        frame.pack()
+        frame_top = ttk.Frame(root)
+        label = ttk.Label(frame_top, text="Window ids: ")
+        entry = ttk.Entry(frame_top)
+        entry.insert(0, "FL,FR")
+        label.pack(side=tk.LEFT, padx=5)
+        entry.pack(side=tk.LEFT, padx=5)
+        frame_top.pack(pady=5)
+
+        frame_bottom = ttk.Frame(root)
+        # Create a style for the progress bars
+        style = ttk.Style()
+        style.configure("Vertical.TProgressbar", thickness=100)  # Set the width here
+        window_widgets = {}
+        for window_id in entry.get().strip().split(","):
+            window_widgets[window_id] = dict()
+            window_widgets[window_id]["pbar_frame"] = pbar_frame = ttk.Frame(frame_bottom)
+            window_widgets[window_id]["pbar"] = pbar = ttk.Progressbar(pbar_frame, orient="vertical", length=200, style="Vertical.TProgressbar", mode="determinate")
+            window_widgets[window_id]["btns_frame"] = btns_frame = ttk.Frame(frame_bottom)
+            window_widgets[window_id]["btn_up"] =   btn_up =   ttk.Button(btns_frame, text="UP (close)",  command=lambda id=window_id: app.send_close(id))
+            window_widgets[window_id]["btn_down"] = btn_down = ttk.Button(btns_frame, text="DOWN (open)", command=lambda id=window_id: app.send_open(id) )
+            pbar.pack(side=tk.LEFT, padx=5, pady=5)
+            pbar_frame.pack(side=tk.LEFT, padx=5, pady=5)
+            btn_up.pack(padx=5, pady=5)
+            btn_down.pack(padx=5, pady=5)
+            btns_frame.pack(side=tk.LEFT, padx=5, pady=5)
+        frame_bottom.pack(pady=5)
+
+        def act_on_samples(samples):
+            for sample in samples:
+                window_widgets[sample.id]["pbar"]["value"] = sample.position
+        
+        app.act_on_samples = act_on_samples
+
         root.mainloop()
     
     else:
