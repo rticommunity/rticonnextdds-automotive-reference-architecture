9ffe94ac07aed34daddbdf7e01f3e10ae168f6e8
Maruel Zain <mzain@rti.com>
2025-02-18 17:53:49 +0100

    Documentation and clean-up of the python code

--- cockpit/gui.py
+++ cockpit/gui.py
@@ -1,10 +1,35 @@
+"""GUI for the Cockpit application.
+
+The CockpitGUI class provides a simple GUI for the Cockpit application,
+which allows the user to control the position of four windows in the 
+window controller example system.
+
+The GUI consists of four vertical progress bars, each representing a window,
+and two buttons for each window to open and close it.
+
+Additionally, the user can click on a progress bar to set the position of the
+corresponding window.
+
+The CockpitGUI class is designed to be used in conjunction with the CockpitConnext
+class, which provides the DDS communication layer for the Cockpit application.
+
+The CockpitApp is the higher level class that ties the CockpitConnext and CockpitGUI
+classes together, and provides the main application logic.
+"""
+
+
 import tkinter as tk
 from tkinter import ttk
 
 
+
 class CockpitGUI:
+    """Simple GUI for the Cockpit application, part of the window
+    control example system."""
 
     def __init__(self):
+        """Create the GUI window and widgets."""
+
         self.root = tk.Tk()
         self.window_widgets = {}
 
@@ -12,9 +37,35 @@ class CockpitGUI:
 
         self.root.title("Cockpit")
         self.root.protocol("WM_DELETE_WINDOW", self.root.destroy)
+    
+
+    def start(self):
+        """Start the GUI main loop."""
+        self.root.mainloop()
+    
+
+    def window_open(self, window_id):
+        """Dummy function, to be overridden by application."""
+        print(f"Opening window {window_id}")
+    
+
+    def window_close(self, window_id):
+        """Dummy function, to be overridden by application."""
+        print(f"Closing window {window_id}")
+    
+
+    def window_set(self, window_id, position):
+        """Dummy function, to be overridden by application."""
+        print(f"Setting window {window_id} to {position}")
+    
+
+    def update_position(self, window_id, position):
+        """Update the position of a window in the GUI."""
+        self.window_widgets[window_id]["pbar"]["value"] = position
 
 
     def _create_widgets(self):
+        """Create the widgets for the GUI."""
         
         frame_top = ttk.Frame(self.root)
         label = ttk.Label(frame_top, text="Window ids: ")
@@ -47,28 +98,10 @@ class CockpitGUI:
 
 
     def _pbar_on_click(self, event, window_id):
+        """Event handler to trigger a window command when clicking on the progress bar."""
+
         pbar = self.window_widgets[window_id]["pbar"]
         pbar_height = pbar.winfo_height()
         click_position = event.y
         new_value = 100 - int((click_position / pbar_height) * 100)
         self.window_set(window_id, new_value)
-    
-
-    def start(self):
-        self.root.mainloop()
-    
-
-    def window_open(self, window_id):
-        print(f"Opening window {window_id}")
-    
-
-    def window_close(self, window_id):
-        print(f"Closing window {window_id}")
-    
-
-    def window_set(self, window_id, position):
-        print(f"Setting window {window_id} to {position}")
-    
-
-    def update_position(self, window_id, position):
-        self.window_widgets[window_id]["pbar"]["value"] = position
